e143455a7519a62927d6055e3634a850f51a22ce
ClumsyKun <464564598@qq.com>
2019-12-30 17:48:20 +0800

    test

--- fastml/decision_tree/test.py
+++ fastml/decision_tree/test.py
@@ -4,17 +4,18 @@ import fastml.core as core
 from decision_tree import DecisionTree
 
 
-watermelon = pd.read_csv('watermelon.csv')
+watermelon = pd.read_csv('fastml/decision_tree/watermelon.csv')
 
 X = watermelon[['色泽','根蒂','敲声','纹理','脐部','触感']].values
 Y = watermelon['类别'].values.astype(np.uint)
 
-core.information_entropy_with_prop(Y, watermelon['色泽'].values.astype(np.uint))
-
-tree = DecisionTree()
-tree.fit(X, Y)
-
-print(1)
+for field in watermelon.columns:
+    print(
+        '%s - %.4f' % (
+            field, 
+            core.information_entropy_with_prop( Y, watermelon[field].values.astype(np.uint) )
+        )
+    )
 
 
 if __name__ == '__main__':
