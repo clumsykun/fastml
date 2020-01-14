2e1d295ad971605b47b8efa64a1a4013f8cbedbd
ClumsyKun <464564598@qq.com>
2020-01-14 11:38:53 +0800

    test

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 import pyextension
+from collections import namedtuple
 
 string_list = ['1123', '上海123', '  ', ',，']
 
 for string in string_list:
     print(string, ': ', pyextension.str_is_pure_ascii(string))
+
+print('\n--------------------------------------------------\n')
+
+Date = namedtuple('DateTime', ['Year', 'Month', 'Day'])
+a = Date(1999, 12, 31)
+pyextension.print_type_name(a)
 
