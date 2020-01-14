55b4213e8afaf1e5e353a1c95a6ec9fdc64ff074
ClumsyKun <464564598@qq.com>
2020-01-14 09:29:59 +0800

    test

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import pyextension
 
-pyextension.test(1)
+string_list = ['1123', '上海123', '  ', ',，']
+
+for string in string_list:
+    print(string, ': ', pyextension.utf8_is_pure_ascii(string))
 
