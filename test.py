436693718861fdeb12d4e6d0235bd66e509fe27a
ClumsyKun <464564598@qq.com>
2020-01-16 15:45:11 +0800

    use wchar_t

--- test.py
+++ test.py
@@ -8,12 +8,6 @@ string_list = ['1123', '上海123', '  ', ',，']
 for string in string_list:
     print(string, ': ', pyextension.str_is_pure_ascii(string))
 
-print('\n--------------------------------------------------\n')
-
-Date = namedtuple('DateTime', ['Year', 'Month', 'Day'])
-a = Date(1999, 12, 31)
-pyextension.print_type_name(a)
-
 
 print('\n--------------------------------------------------\n')
 
